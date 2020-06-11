0379317f0885e4ffbc2c79f5ac42979d2b119329
haneefnani <55850551+haneefnani@users.noreply.github.com>
2020-06-11 11:28:54 +0530

    project 7 update

---
+++ Project-8 LED Intensity Control/led_brightness.py
@@ -0,0 +1,11 @@
+from boltiot import Bolt
+bolt = open(r'C:\Users\haneef\Documents\Cerdentials\bolt_iot.txt').read()
+device_id, api_key = bolt.split()
+
+mybolt = Bolt(api_key, device_id)
+
+# pin 0 & intensity level 10
+# Intensity ranges from (0,255)
+
+response = mybolt.analogWrite('0', '10')
+print(response)
