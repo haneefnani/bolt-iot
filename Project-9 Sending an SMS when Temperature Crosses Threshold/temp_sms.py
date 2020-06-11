a9252860ceaefea012c97c564d24ad73a29c9d08
haneefnani <55850551+haneefnani@users.noreply.github.com>
2020-06-11 13:35:25 +0530

    Project 9 Completed....

--- Project-9 Sending an SMS when Temperature Crosses Threshold/temp_sms.py
+++ Project-9 Sending an SMS when Temperature Crosses Threshold/temp_sms.py
@@ -3,8 +3,9 @@ from boltiot import Sms, Bolt
 import json
 import time
 
-minimum_limit = 300
-maximum_limit = 320
+
+minimum_limit = 30  # minimum temperature limit
+maximum_limit = 39  # maximum temperature limit
 
 
 mybolt = Bolt(conf.API_KEY, conf.DEVICE_ID)
@@ -15,19 +16,18 @@ while True:
     print("Reading sensor value")
     response = mybolt.analogRead('A0')
     data = json.loads(response)
-    print("Sensor value is: " + str(data['value']))
+    sensor_value = int(data['value'])
+    Temperature = (100*sensor_value)/1024
+    print(f"Temperature is :  {Temperature: .2f}")
+    #print("Sensor value is: " + str(data['value']))
     try:
-        sensor_value = int(data['value'])
-        if sensor_value > maximum_limit or sensor_value < minimum_limit:
+        if Temperature > maximum_limit or Temperature < minimum_limit:
             print("Making request to Twilio to send a SMS")
             response = sms.send_sms(
-                "The Current temperature sensor value is " + str(sensor_value))
+                f"The Current temperature is :{Temperature: .2f}")
             print("Response received from Twilio is: " + str(response))
             print("Status of SMS at Twilio is :" + str(response.status))
     except Exception as e:
         print("Error occured: Below are the details")
         print(e)
     time.sleep(10)
-
-Temperature = (100*sensor_value)/1024
-print(Temperature)
