a37bf834ec820a71e143634f3031fd68dea7d23d
haneefnani <55850551+haneefnani@users.noreply.github.com>
2020-07-13 21:13:51 +0530

    Revert "Merge branch 'master' of https://github.com/haneefnani/bolt-iot"
    This reverts commit 30fc37e29d15853b844808f66f9bc4dc3f777527, reversing
    changes made to c482b8fe7da766cd3513715afdfac84e4f9465a5.

---
+++ Project-11 Send Telegram message when temperature crosses threshold/conf.py
@@ -0,0 +1,10 @@
+"""Configurations for telegram_alert.py"""
+bolt_api_key = "876619b0-11e9-41e4-9206-c70652dcaac0"                 # This is your Bolt Cloud API Key
+# This is the device ID and will be similar to BOLTXXXX where XXXX is some numbers
+device_id = "BOLT1349843"
+# This is the channel ID of the created Telegram channel. Paste after @ symbol.
+telegram_chat_id = "@bolt_iot_temperature_monitor"
+# This is the bot ID of the created Telegram Bot. Paste after bot text.
+telegram_bot_id = "bot935754143:AAEePbgKjm3pjO07so34Klrf5xd_CcrLapQ"
+threshold = 250                       # Threshold beyond which the alert should be sent
+  
